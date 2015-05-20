4ff5ac52f57ad8496b365659e5bbec87c9b93c4b
Edwin Ortiz <eortiz@udel.edu>
2015-05-20 17:53:20 -0400

    added hashset

--- Question1Final/src/TestVehicle.java
+++ Question1Final/src/TestVehicle.java
@@ -1,17 +1,27 @@
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.UUID;
 
-
 public class TestVehicle {
-	
+
 	public static void main(String[] args) {
-		Person p = new Person("p", "body", new UUID (10, 10));
+		Person p = new Person("p", "body", new UUID(10, 10));
 		Car c = new Car("c", "red", 1200, p, 4);
 		Truck t = new Truck("c", "blue", 4000, p, 6);
-		
-		Person b = new Person("b", "bro", new UUID (10, 10));
-		
+
+		Person b = new Person("b", "bro", new UUID(10, 10));
+
 		MotorCycle m = new MotorCycle("m", "black", 400, b, false);
 		t.transferOwnership(b);
+
+		ArrayList<Vehicle> Vehicles = new ArrayList<Vehicle>();
+
+		Vehicles.add(c);
+		Vehicles.add(t);
+		Vehicles.add(m);
+
+		HashSet VehicleHS = new HashSet(Vehicles);
+
 	}
-	
+
 }
